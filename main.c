e047f9d559d92f95524cd0812f1d8c8cc2cb5d0c
Angelina Orlova <111185483+vngelline@users.noreply.github.com>
2024-03-03 18:04:44 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,9 +6,14 @@
 /*   By: vngelline <vngelline@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 09:37:21 by vngelline         #+#    #+#             */
-/*   Updated: 2024/03/03 13:31:41 by vngelline        ###   ########.fr       */
+/*   Updated: 2024/03/03 15:00:05 by vngelline        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+//void rush(int x, int y);
+
+void    ft_putchar(char c);
+void    rush(int y_count, int x_count);
 
 int main(void) 
 {
